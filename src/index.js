025c3ab3b2e0de41d15543f6941bed9271afe542
Wei Zhu <yesmeck@gmail.com>
2018-02-13 13:59:58 +0800

    Fix binding judging

--- src/index.js
+++ src/index.js
@@ -17,21 +17,12 @@ export default function ({ types }) {
     }
   }
 
-  function Program(path, { opts = {} }) {
-    // Init plugin instances once.
-    if (!plugins) {
-      if (Array.isArray(opts)) {
-        plugins = opts.map(({
-          libraryName,
-          libraryDirectory,
-          style,
-          camel2DashComponentName,
-          camel2UnderlineComponentName,
-          fileName,
-          customName,
-        }) => {
-          assert(libraryName, 'libraryName should be provided');
-          return new Plugin(
+  const Program = {
+    enter(path, { opts = {} }) {
+      // Init plugin instances once.
+      if (!plugins) {
+        if (Array.isArray(opts)) {
+          plugins = opts.map(({
             libraryName,
             libraryDirectory,
             style,
@@ -39,27 +30,41 @@ export default function ({ types }) {
             camel2UnderlineComponentName,
             fileName,
             customName,
-            types
-          );
-        });
-      } else {
-        assert(opts.libraryName, 'libraryName should be provided');
-        plugins = [
-          new Plugin(
-            opts.libraryName,
-            opts.libraryDirectory,
-            opts.style,
-            opts.camel2DashComponentName,
-            opts.camel2UnderlineComponentName,
-            opts.fileName,
-            opts.customName,
-            types
-          ),
-        ];
+          }) => {
+            assert(libraryName, 'libraryName should be provided');
+            return new Plugin(
+              libraryName,
+              libraryDirectory,
+              style,
+              camel2DashComponentName,
+              camel2UnderlineComponentName,
+              fileName,
+              customName,
+              types
+            );
+          });
+        } else {
+          assert(opts.libraryName, 'libraryName should be provided');
+          plugins = [
+            new Plugin(
+              opts.libraryName,
+              opts.libraryDirectory,
+              opts.style,
+              opts.camel2DashComponentName,
+              opts.camel2UnderlineComponentName,
+              opts.fileName,
+              opts.customName,
+              types
+            ),
+          ];
+        }
       }
-    }
-    applyInstance('Program', arguments, this);  // eslint-disable-line
-  }
+      applyInstance('ProgramEnter', arguments, this);  // eslint-disable-line
+    },
+    exit() {
+      applyInstance('ProgramExit', arguments, this);  // eslint-disable-line
+    },
+  };
 
   const methods = [
     'ImportDeclaration',
