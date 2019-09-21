36dfb60a54f1fd4e3a6434b2ec6e82ed34c714e9
zhengqc <zhengqc@seagroup.com>
2019-09-21 14:28:15 +0800

    fix: local scope bug

--- test/fixtures/variable-scope/expected.js
+++ test/fixtures/variable-scope/expected.js
@@ -2,18 +2,20 @@
 
 var _react = _interopRequireDefault(require("react"));
 
-var _message2 = _interopRequireDefault(require("antd/lib/message"));
+var _message3 = _interopRequireDefault(require("antd/lib/message"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _message2.default)('xxx');
+(0, _message3.default)('xxx');
+
+_message3.default.error('error');
 
 var testIf = function testIf(message) {
   if (message) return message;
 };
 
 var testIf2 = function testIf2() {
-  if (_message2.default) return _message2.default;
+  if (_message3.default) return _message3.default;
 };
 
 var testExpression = function testExpression(message) {
@@ -21,7 +23,7 @@ var testExpression = function testExpression(message) {
 };
 
 var testExpression2 = function testExpression2() {
-  return _message2.default + 'test';
+  return _message3.default + 'test';
 };
 
 var testNestFunction = function testNestFunction(message) {
@@ -32,8 +34,38 @@ var testNestFunction = function testNestFunction(message) {
 
 var testNestFunction2 = function testNestFunction2() {
   return function (a) {
-    return _message2.default;
+    return _message3.default;
   };
+};
+
+var testFunction = function testFunction(message) {
+  message.error('error');
+  return message.test;
+};
+
+var testFunction1 = function testFunction1() {
+  return _message3.default.error('error');
+};
+
+var testFunction2 = function testFunction2(message) {
+  return message.error('error');
+};
+
+var testFunction3 = function testFunction3(message) {
+  if (message) {
+    message = message.test.message;
+
+    for (var i = 0; i < 10; i++) {
+      var _message2 = i;
+
+      if (_message2 > 4) {
+        return _message2;
+      }
+    }
+  }
+
+  message = null;
+  return message;
 };
 
 function App() {
