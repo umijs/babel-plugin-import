7a7f34c367fadeb6963b978800b1b7477b71523a
华志林 <linmu.hzl@raycloud.com>
2021-10-31 21:50:39 +0800

    fix: newExpression arguments not be traverse issue

--- test/fixtures/as-arguments-identifier/expected.js
+++ test/fixtures/as-arguments-identifier/expected.js
@@ -5,3 +5,5 @@ var _modal = _interopRequireDefault(require("antd/lib/modal"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _Modal = bind()(_modal.default);
+
+var _newModal = new AnyClass(_modal.default);
